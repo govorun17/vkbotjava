68f7ac885dd62c263dbd3c9c1a682ac7d455e421
Aleksey Mazurov <voidzarik@gmail.com>
2020-08-07 13:11:22 +0300

    init commit

---
+++ src/main/java/my/test/vkbotspring/VkbotspringApplication.java
@@ -0,0 +1,13 @@
+package my.test.vkbotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkbotspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkbotspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/test/vkbotspring/VkbotspringApplicationTests.java
@@ -0,0 +1,13 @@
+package my.test.vkbotspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkbotspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
